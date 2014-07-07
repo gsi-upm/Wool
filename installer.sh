35ee44dc9a35403ccf21a57941bc068c9573268d
oaraque <oaraque@homer.(none)>
2014-07-07 14:11:48 +0200

    Added installer.sh

---
+++ installer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Installing Wool..."
+WOOL_DIR=$(pwd)
+echo $WOOL_DIR
+cd ~
+mkdir core
+git clone https://github.com/fuel/core.git
+cd core
+rm .git 
+cd ..
+cp -r core WOOL_DIR
+exit
